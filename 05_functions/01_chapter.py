4b7d2d0cca89b2ca1f9b941a79aadb0b58d09c2e
abhinav <d28.abhinav@gmail.com>
2026-09-22 11:09:42 +0530

    writing the first funcation

---
+++ 05_functions/01_chapter.py
@@ -0,0 +1,22 @@
+def print_order(name,chai_type):
+    print(f"{name}  orderfood {chai_type}")
+
+
+print_order("aman","masala")
+
+
+def fetch_sales():
+    print('fetch sales data')
+
+
+def filter_valid_sales():
+    print("filtering the  valid sales data")
+
+def summarize_data():
+    print("Summarize sales data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+
