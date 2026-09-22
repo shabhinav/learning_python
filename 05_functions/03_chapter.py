ad5106f642c99e53104b921e80b56a2903f4e9d9
abhinav <d28.abhinav@gmail.com>
2026-09-22 11:43:49 +0530

    non local and global variable update

---
+++ 05_functions/03_chapter.py
@@ -0,0 +1,14 @@
+def serve_chai():
+    chai_type = "Masala"
+    print(f"Inside funcation {chai_type}")
+
+
+    def chai_type():
+        global new_chai_type
+        new_chai_type  = "TULSI"
+        chai = "masala"
+        def new_chai_type():
+            nonlocal chai
+            chai = "Ginger"
+
+
